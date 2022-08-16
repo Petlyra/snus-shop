cc82f406f0944896935aed7276dc8d7284133ea4
Petlyra <poley.yuri@gmail.com>
2022-08-16 17:54:25 +0300

    trust wallet core

--- android/app/src/main/kotlin/paley/yuri/snus_shop/MainActivity.kt
+++ android/app/src/main/kotlin/paley/yuri/snus_shop/MainActivity.kt
@@ -2,5 +2,8 @@ package paley.yuri.snus_shop
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:snus_shop/page/home.dart';
+import 'package:trust_wallet_core/flutter_trust_wallet_core.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +10,110 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Snus Shop',
-      theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.black,
-        foregroundColor: Colors.white,
-      )),
-      home: const Home(),
+    // return MaterialApp(
+    //   title: 'Snus Shop',
+    //   theme: ThemeData(
+    //       appBarTheme: const AppBarTheme(
+    //     backgroundColor: Colors.black,
+    //     foregroundColor: Colors.white,
+    //   )),
+    //   home: const Home(),
+    // );
+    return const MaterialApp(
+      home: TestWallet(),
+    );
+  }
+}
+
+class TestWallet extends StatefulWidget {
+  const TestWallet({Key? key}) : super(key: key);
+
+  @override
+  State<TestWallet> createState() => _TestWalletState();
+}
+
+class _TestWalletState extends State<TestWallet> {
+  late HDWallet wallet;
+
+  @override
+  void initState() {
+    FlutterTrustWalletCore.init();
+    super.initState();
+    String mnemonic =
+        "horror select baby exile convince sunset outside vehicle write decade powder energy"; // 有测试币的 tron地址
+    wallet = HDWallet.createWithMnemonic(mnemonic);
+  }
+
+  Widget _exampleItem({
+    required String name,
+    required WidgetBuilder builder,
+  }) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: builder));
+      },
+      child: Text(name),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('wallet core example app'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(wallet.mnemonic()),
+            // Expanded(
+            //   child: ListView(
+            //     padding: EdgeInsets.zero,
+            //     children: [
+            //       _exampleItem(
+            //         name: 'Ethereum',
+            //         builder: (_) {
+            //           return EthereumExample(wallet);
+            //         },
+            //       ),
+            //       _exampleItem(
+            //         name: 'Bitcoin Address',
+            //         builder: (_) {
+            //           return BitcoinAddressExample(wallet);
+            //         },
+            //       ),
+            //       _exampleItem(
+            //         name: 'Bitcoin Transaction',
+            //         builder: (_) {
+            //           return BitcoinTransactionExample(wallet);
+            //         },
+            //       ),
+            //       _exampleItem(
+            //         name: 'Tron',
+            //         builder: (_) {
+            //           return TronExample(wallet);
+            //         },
+            //       ),
+            //       _exampleItem(
+            //         name: 'PrivateKey.isValid(a,b)',
+            //         builder: (_) {
+            //           return PrivateKeyIsValidExample(wallet);
+            //         },
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            ElevatedButton(
+              onPressed: () async {
+                wallet.delete();
+                wallet = HDWallet();
+                setState(() {});
+              },
+              child: const Text("gen"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
